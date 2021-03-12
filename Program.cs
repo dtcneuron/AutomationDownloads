d2f2fa42f2e10d80c75b61416837250e88436414
dtcneuron <DTC@mw2.uk>
2021-03-12 16:49:22 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomationDownloads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
